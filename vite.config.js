948cf270dadbf253111f988a6cefd05ee0cbf7b5
vashasashenka <145197914+vashasashenka@users.noreply.github.com>
2026-04-13 22:10:23 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 5173,
+  build: {
+    rollupOptions: {
+      output: {
+        // Это заставит Vercel создать файлы с НОВЫМИ именами
+        assetFileNames: 'assets/[name]-[hash]-v2[extname]',
+        chunkFileNames: 'assets/[name]-[hash]-v2.js',
+        entryFileNames: 'assets/[name]-[hash]-v2.js',
+      },
+    },
   },
 })
